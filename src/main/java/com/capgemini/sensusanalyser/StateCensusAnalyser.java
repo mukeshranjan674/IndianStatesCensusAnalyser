08ec09364083726bf73f8395df49ba5415594e8b
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-24 22:05:42 +0530

    [Mukesh] add : StateCensusAnalyser Class written

---
+++ src/main/java/com/capgemini/sensusanalyser/StateCensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.capgemini.sensusanalyser;
+
+public class StateCensusAnalyser {
+	public static void main(String[] args) {
+		System.out.println("*****Welcome to State Census Analyser*****");
+	}
+}
