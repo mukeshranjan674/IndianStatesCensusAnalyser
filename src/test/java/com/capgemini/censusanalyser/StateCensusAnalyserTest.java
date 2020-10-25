394e333a550463de391ce083934fc66a974d3e3e
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-25 11:41:50 +0530

    [Mukesh] add : TC 1.3 Validate with bad csv file

--- src/test/java/com/capgemini/censusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/capgemini/censusanalyser/StateCensusAnalyserTest.java
@@ -10,31 +10,44 @@ import com.capgemini.sensusanalyser.StateCensusAnalyser;
 
 public class StateCensusAnalyserTest {
 
-	private static final String STATE_CSV_DATA = "C:\\Users\\LENOVO\\eclipse-workspace\\IndianStateCensusAnalyser" + 
-												 "\\src\\main\\resources\\StateCensusData.csv";
-	private static final String WRONG_FILE = "C:\\Users\\LENOVO\\eclipse-workspace\\StateCensusData.txt";
+	private static final String STATE_CENSUS_CSV_FILE_PATH = "./src/test/resources/StateCensusData.csv";
+	private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/StateCensusData.csv";
+	private static final String CENSUS_CSV_FILE_PATH = "./src/test/resources/CensusData.csv";
 
 	@Test
 	public void whenGivenCsvPathShouldReturnNumberOfRecords() {
 		try {
 			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
-			int records;
-			records = stateCensusAnalyser.loadIndiaCensusData(STATE_CSV_DATA);
+			int records = stateCensusAnalyser.loadIndiaCensusData(STATE_CENSUS_CSV_FILE_PATH);
 			assertEquals(28, records);
+			System.out.println(records);
 		} catch (CensusAnalyserException e) {
 			e.getMessage();
 		}
 	}
 
 	@Test
-	public void whenGivenWrongCsvFileShouldThrowCustomException() {
+	public void whenGivenWrongFileShouldThrowCustomException() {
 		try {
 			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
 			ExpectedException exceptionRule = ExpectedException.none();
 			exceptionRule.expect(CensusAnalyserException.class);
-			stateCensusAnalyser.loadIndiaCensusData(WRONG_FILE);
+			stateCensusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
 		} catch (CensusAnalyserException e) {
-			assertEquals(CensusAnalyserException.ExceptionType.NO_FILE_FOUND, e.type);
+			assertEquals(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, e.type);
+		}
+	}
+
+	@Test
+	public void whenGivenRightCsvFileButWrongTypeShouldThrowCustomException() {
+		try {
+			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CensusAnalyserException.class);
+			int records = stateCensusAnalyser.loadIndiaCensusData(CENSUS_CSV_FILE_PATH);
+			System.out.println(records);
+		} catch (CensusAnalyserException e) {
+			assertEquals(CensusAnalyserException.ExceptionType.WRONG_CSV_DATA, e.type);
 		}
 	}
 }
