151337e11f1e2702c35f399ba154d905d292b3ea
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-25 11:43:21 +0530

    [Mukesh] Refactor : Code refactored to pass TC 1.3

--- src/main/java/com/capgemini/sensusanalyser/CSVStateCensus.java
+++ src/main/java/com/capgemini/sensusanalyser/CSVStateCensus.java
@@ -5,6 +5,7 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 import com.capgemini.sensusanalyser.CensusAnalyserException.ExceptionType;
@@ -16,15 +17,21 @@ public class CSVStateCensus {
 	public int loadIndiaCensusData(String STATE_CSV_DATA) throws CensusAnalyserException {
 		int numOfRecords = 0;
 		try (Reader reader = Files.newBufferedReader(Paths.get(STATE_CSV_DATA));) {
-			CsvToBean<StateCensusData> csvToBean = new CsvToBeanBuilder<StateCensusData>(reader)
-					.withType(StateCensusData.class).withIgnoreLeadingWhiteSpace(true).build();
-
+			CsvToBeanBuilder<StateCensusData> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			csvToBeanBuilder.withType(StateCensusData.class);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<StateCensusData> csvToBean = csvToBeanBuilder.build();
 			Iterator<StateCensusData> csvCensusIterator = csvToBean.iterator();
 			Iterable<StateCensusData> censusIterable = () -> csvCensusIterator;
 			numOfRecords = (int) StreamSupport.stream(censusIterable.spliterator(), false).count();
+			return numOfRecords;
 		} catch (IOException e) {
-			throw new CensusAnalyserException(e.getMessage(), ExceptionType.NO_FILE_FOUND);
+				throw new CensusAnalyserException(e.getMessage(), ExceptionType.FILE_NOT_FOUND);
+		}catch (IllegalStateException e) {
+				throw new CensusAnalyserException(e.getMessage(), ExceptionType.UNABLE_TO_PARSE);
+		}catch (RuntimeException e) {
+				throw new CensusAnalyserException(e.getMessage(), ExceptionType.WRONG_CSV_DATA);
 		}
-		return numOfRecords;
+		
 	}
 }
--- src/main/java/com/capgemini/sensusanalyser/StateCensusData.java
+++ src/main/java/com/capgemini/sensusanalyser/StateCensusData.java
@@ -7,7 +7,7 @@ public class StateCensusData {
 	@CsvBindByName(column = "State")
 	private String state;
 
-	@CsvBindByName(column = "Population")
+	@CsvBindByName(column = "Population", required = true )
 	private int population;
 
 	@CsvBindByName(column = "AreaInSqKm")
@@ -15,37 +15,4 @@ public class StateCensusData {
 
 	@CsvBindByName(column = "DensityPerSqKm")
 	private int densityPerSqKm;
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public int getDensityPerSqKm() {
-		return densityPerSqKm;
-	}
-
-	public void setDensityPerSqKm(int densityPerSqKm) {
-		this.densityPerSqKm = densityPerSqKm;
-	}
-
-	public int getAreaInSqKm() {
-		return areaInSqKm;
-	}
-
-	public void setAreaInSqKm(int areaInSqKm) {
-		this.areaInSqKm = areaInSqKm;
-	}
-
-	public int getPopulation() {
-		return population;
-	}
-
-	public void setPopulation(int population) {
-		this.population = population;
-	}
-
 }
